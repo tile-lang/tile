c9a7868d0e7ee19d5fbbae98b6e3b5d2ac441fe0
Muhammed Yasinhan Yaşar <sesden19611@gmail.com>
2024-12-09 22:22:13 +0300

    token tpyes and symbol table check for debug purposes

--- src/tile/app/Tile.java
+++ src/tile/app/Tile.java
@@ -1,16 +1,19 @@
 package tile.app;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 
 import gen.antlr.tile.tileLexer;
 import gen.antlr.tile.tileParser;
 import tile.AntlrToProgram;
 import tile.Program;
+import tile.ast.stmt.FunctionDefinition;
 
 public class Tile {
     public static void main(String args[]) {
@@ -20,6 +23,11 @@ public class Tile {
         Program program = programVisitor.visit(ast);
 
         program.generate();
+
+        System.out.println(FunctionDefinition.funcDefSymbols.keySet());
+        for (FunctionDefinition value : FunctionDefinition.funcDefSymbols.values()) {
+            System.out.println(value.variableSymbols.keySet());
+        }
     }
 
     private static tileParser createTileParser(String filePath) {
@@ -29,6 +37,14 @@ public class Tile {
             CharStream input = CharStreams.fromFileName(filePath);
             tileLexer lexer = new tileLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
+            
+            // // Force the token stream to fetch all tokens
+            // tokens.fill();
+            // // Print tokens and their types
+            // for (Token token : tokens.getTokens()) {
+            //     System.out.println(token.getText() + " -> " + token.getType());
+            // }
+
             parser = new tileParser(tokens);
         } catch (IOException e) {
             e.printStackTrace();
